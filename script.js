2bb63a6108b990beefd3d72924cbda0599deb2db
SujanDevaraj <96370355+SujanDevaraj@users.noreply.github.com>
2023-09-20 20:19:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-  function updateTimer() {
+  /*function updateTimer() {
             const timerElement = document.getElementById('timer');
             const now = new Date();
             const formattedDate = now.toLocaleDateString();
@@ -11,4 +11,28 @@
         setInterval(updateTimer, 1000);
 
         // Initial call to display the timer
-        updateTimer();
+        updateTimer();*/
+
+  function updateTimer() {
+      const timerElement = document.getElementById("timer");
+      const currentDate = new Date();
+      
+      const options = {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: true,
+      };
+      
+      const formattedDate = currentDate.toLocaleString(undefined, options);
+      timerElement.textContent = ` ${formattedDate}`;
+    }
+
+    // Update the timer every second
+    setInterval(updateTimer, 1000);
+
+    // Initial update
+    updateTimer();
